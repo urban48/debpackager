5eeffe8380a2d5a26c19fd6bc4d55ea30dfbbbca
Sergey R <segrog@gmail.com>
2016-07-11 23:23:20 +0200

    tests added

--- debpackager/packages/general_package.py
+++ debpackager/packages/general_package.py
@@ -68,7 +68,8 @@ class GeneralPackage(object):
 
         deb_dependencies = self.extra_args.get('pom', {}) \
             .project.get('deb_dependencies')
-        for deb in self.extra_args.get('pom', {}).project.get(
+        project = self.extra_args.get('pom', {}).project
+        for deb in project.get(
                 'debians', []):
             deb_name = deb.get('name', self.project_name)
             dpm = Dpm(project_path=self.project_path,
@@ -76,7 +77,7 @@ class GeneralPackage(object):
                       package_version=self.new_version,
                       install_path=deb.get('install_path'),
                       dependencies=deb_dependencies,
-                      excludes=deb.get('excludes'))
+                      excludes=project.get('excludes', []))
 
             build_dir = dpm.build()
 
